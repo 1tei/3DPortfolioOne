fb80187e6ed4663e652a923d72ab2419d40135db
1tei <reinis.tei@gmail.com>
2024-02-22 22:38:51 +0200

    Created a 3D island portfolio with multiple points that lead to seperate routes.

---
+++ src/models/Plane.jsx
@@ -0,0 +1,21 @@
+import { useRef, useEffect } from 'react'
+import { useAnimations, useGLTF } from '@react-three/drei'
+import planeScene from '../assets/3d/plane.glb'
+
+const Plane = ({ isRotating, ...props }) => {
+  const ref = useRef();
+  const { scene, animations } = useGLTF(planeScene);
+  const { actions } = useAnimations(animations, ref);
+
+  useEffect(() =>  {
+      actions['Take 001'].play();
+  }, [actions, isRotating])
+
+  return (
+    <mesh {...props} ref={ref}> 
+        <primitive object={scene} /> 
+    </mesh>
+  )
+}
+
+export default Plane
